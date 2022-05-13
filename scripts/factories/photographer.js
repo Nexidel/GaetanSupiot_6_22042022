883d0e68b0ae579daa7f64f70482acfb619265e1
unknown <gaetan.supiot@gmail.com>
2022-05-13 13:19:51 +0200

    Retrive data from json | adapt js to show data on index

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,20 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, price, tagline} = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
+        const dataCollect = document.createElement( 'span' );
+        h2.textContent = name ;
+        dataCollect.textContent = "Données du fetch : " + city + ", " + country + ", " + price + ", " + tagline
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(dataCollect);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, price, tagline, getUserCardDOM }
 }
