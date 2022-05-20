a9367c320b2c579c38c645397304b5f70ea2eef8
unknown <gaetan.supiot@gmail.com>
2022-05-20 12:07:54 +0200

    add element to create for photographers content + accessibility

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,19 +1,34 @@
 function photographerFactory(data) {
-    const { name, portrait, city, country, price, tagline} = data;
+    const { name, portrait, city, country, price, tagline } = data;
 
     const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        const img = document.createElement('img');
+        img.setAttribute('alt', '')
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        const dataCollect = document.createElement( 'span' );
-        h2.textContent = name ;
-        dataCollect.textContent = "Données du fetch : " + city + ", " + country + ", " + price + ", " + tagline
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(dataCollect);
+        const aPhotographer = document.createElement('a')
+        const h2 = document.createElement('h2');
+        const divPhotographerData = document.createElement('div')
+        divPhotographerData.setAttribute('id', "desc_" + name)
+        const divPhotographerCity = document.createElement('p');
+        const divPhotographerTagline = document.createElement('p');
+        const divPhotographerPrice = document.createElement('p');
+        h2.textContent = name;
+        divPhotographerCity.textContent = city + " " + country
+        divPhotographerTagline.textContent = tagline
+        divPhotographerPrice.textContent = price + " €/jour"
+        article.appendChild(aPhotographer);
+        article.appendChild(divPhotographerData);
+        // Ajoute un id avec le nom du photographe 
+        aPhotographer.setAttribute('id', name)
+        aPhotographer.appendChild(img);
+        aPhotographer.appendChild(h2);
+        article.appendChild(divPhotographerData);
+        divPhotographerData.appendChild(divPhotographerCity);
+        divPhotographerData.appendChild(divPhotographerTagline);
+        divPhotographerData.appendChild(divPhotographerPrice);
         return (article);
     }
     return { name, picture, city, country, price, tagline, getUserCardDOM }
